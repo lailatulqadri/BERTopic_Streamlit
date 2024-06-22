7f17787e1a04071754ba8c3b2cee18677a90b477
Lailatul Qadri Zakaria <61860775+lailatulqadri@users.noreply.github.com>
2024-06-22 11:38:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,9 +17,9 @@ def load_data(uploaded_file):
 
 # Perform BERTopic modeling
 #@st.cache_resource
-def create_model(data, n_gram_range=(1, 3),num_topics, min_topic_size, nr_topics):
+def create_model(data,num_topics, min_topic_size, nr_topics):
     texts = data['text'].tolist()
-    model = BERTopic(n_gram_range=n_gram_range, calculate_probabilities=True,nr_topics=num_topics, min_topic_size=min_topic_size, nr_top_words=nr_topics)
+    model = BERTopic(calculate_probabilities=True,nr_topics=num_topics, min_topic_size=min_topic_size, nr_top_words=nr_topics)
     topics, _ = model.fit_transform(texts)
     st.write(model.get_topic_info())
     fig = model.visualize_topics()
